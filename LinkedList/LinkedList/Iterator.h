3a4eff5165bb4f5724c9e1aec7939db1d4f019e7
Justin Judice <jjudice51@gmail.com>
2024-01-31 12:12:27 -0600

    finished building the functions out in header portions

--- LinkedList/LinkedList/Iterator.h
+++ LinkedList/LinkedList/Iterator.h
@@ -7,17 +7,54 @@ class Iterator
 
 public:
 
-	Iterator()
-	{
+	Iterator();
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="node"></param>
+	Iterator(Node<AnyType>* node);
 
-	}
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns></returns>
+	Iterator<AnyType> operator ++();
 	
-	operator ++() : Iterator<AnyType>;
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns></returns>
+	Iterator<AnyType> operator --();
+	
+	/// <summary>
+	///
+	/// </summary>
+	/// <param name="iter"></param>
+	/// <returns></returns>
+	const bool operator ==(const Iterator<AnyType>& iter);
+	
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="iter"></param>
+	/// <returns></returns>
+	const bool operator !=(const Iterator<AnyType>& iter);
+	
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns></returns>
+	AnyType operator *();
+
 
 
 
 private:
 
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <typeparam name="AnyType"></typeparam>
 	Node m_current
 
 
--- LinkedList/LinkedList/LinkedList.h
+++ LinkedList/LinkedList/LinkedList.h
@@ -6,10 +6,42 @@ class LinkedList
 {
 public:
 
-	LinkedList()
-	{
+	LinkedList();
+	LinkedList(const LinkedList<AnyType>& other);
+	~LinkedList();
 
-	}
+	void initialize();
+	void destroy();
+
+	Iterator<AnyType> begin() const;
+	Iterator<AnyType> end()  const;
+
+	AnyType first() const;
+	AnyType last() const;
+
+	bool contains(const AnyType object) const;
+	bool isEmpty() const;
+	void print() const;
+
+	void pushFront(const AnyType& value);
+	void PushBack(const AnyType& value);
+
+	AnyType PopFront();
+	AnyType PopBack();
+
+	bool insert(const AnyType& value, int index);
+	bool remove(const AnyType& value);
+
+	const void print();
+	void initialize();
+	const bool isEmpty();
+	bool getData(Iterator<AnyType>& iter, int index);
+	const bool getLength();
+	
+	void operator =(const LinkedList<AnyType>& otherLinkedList);
+
+	void sort();
+
 
 
 
@@ -27,8 +59,8 @@ private:
 
 };
 
-
-
-
-
-
+template<typename AnyType>
+inline LinkedList<AnyType>::LinkedList(const LinkedList<AnyType>& other)
+{
+	*this = other;
+}
